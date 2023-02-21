cd7f19dbc82f8c725f546b102fe2198a8fa38afe
Pradeep241094 <pradeepprakash24@gmail.com>
2023-02-20 19:22:14 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chatgpt-whatsapp-integration
---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { Configuration, OpenAIApi } = require("openai");
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY
+});
+
+
+const openai = new OpenAIApi(configuration);
+
+client.on('message', message => {
+    console.log(message.body);
+
+    if(message.body.startsWith("#")) {
+        runCompletion(message.body.substring(1)).then(result => message.reply(result));
+    }
+});
+
+async function runCompletion (message) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
